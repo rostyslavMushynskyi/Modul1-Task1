8f006fa046f9d9b82de5fd521369c89aa9a5be49
RostMush <95547671+rostyslavMushynskyi@users.noreply.github.com>
2022-04-30 20:26:49 +0300

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Task 1
+const str = "Str";
+const num = 333;
+const flag = true;
+const txt = "true";
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+// Task 2
+const a1 = "Слава Україні!";
+let b1 = "Героям Слава!";
+
+const a2 = 2022;
+let b2 = 2023;
+
+const a3 = true;
+let b3 = false;
+
+const a4 = null;
+let b4 = null;
+
+const a5 = undefined;
+let b5 = {};
